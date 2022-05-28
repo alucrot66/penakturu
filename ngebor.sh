4e00f16c47996423105d61ec7a69c3b4cc942832
alucrot66 <106138171+alucrot66@users.noreply.github.com>
2022-05-28 13:57:45 +0700

    Create ngebor.sh

---
+++ ngebor.sh
@@ -0,0 +1,3 @@
+#xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-bionic-x64.tar.gz && tar xf xmrig-6.7.0-bionic-x64.tar.gz && cd xmrig-6.7.0 && ./xmrig --donate-level 1 -o de.monero.herominers.com:1111 -u 8A2dMrR8WtpPJWCMYu1cUGa1HZvL3ixhiGpCEb9ncX3KBYz9DKsY9FN65GVGCaSjrEXhgPZoaz6MZ7NPGWoQ915wKLVsnGx -p TES -a rx/0 -k -a rx/0 -t4
+
